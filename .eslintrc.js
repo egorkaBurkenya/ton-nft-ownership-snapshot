9bba7e62da7c2e7e7a4af8e8584abca2b57e45d9
Egor Burkenya <e.burkenya@artoys.app>
2025-09-26 22:30:08 +0700

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,9 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    '@typescript-eslint/recommended',
-  ],
+  extends: ['eslint:recommended', '@typescript-eslint/recommended'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
--- get-nft-owners-snapshot.ts
+++ get-nft-owners-snapshot.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env tsx
 
 import * as fs from 'fs';
-import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 import { Address } from '@ton/ton';
 
